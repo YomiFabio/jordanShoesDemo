803d4b0422a7187547a7d2aa0235044e8f54c772
Fábio <fabio.dev07@gail.com>
2021-10-15 01:02:41 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+        const sr = ScrollReveal({
+                origin: 'top',
+                distance: '50px',
+                duration: 800,
+                reset: true
+            });
+    
+            ScrollReveal().reveal('.container', { delay: 200 });
+            ScrollReveal().reveal('.destaques', { delay: 600 });
+            ScrollReveal().reveal('.main', { delay: 700 });
+
+
+var typed = new Typed('#typed', {
+  strings: ["#typed-strings"],
+  typeSpeed: 30
+});
