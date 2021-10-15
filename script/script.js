ca479ee1d1913c1f4f0a6af329970c3f83de946c
YomiFabio <92472070+YomiFabio@users.noreply.github.com>
2021-10-15 01:06:21 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -8,9 +8,3 @@
             ScrollReveal().reveal('.container', { delay: 200 });
             ScrollReveal().reveal('.destaques', { delay: 600 });
             ScrollReveal().reveal('.main', { delay: 700 });
-
-
-var typed = new Typed('#typed', {
-  strings: ["#typed-strings"],
-  typeSpeed: 30
-});
